2de5f31dd126b660e5c97408e4b910a3b422a1f9
Mosho <moises.leon@chasacademy.se>
2025-11-18 14:35:53 +0100

    Adjusted base and changed to hasrouter

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [
     react({
       babel: {
@@ -10,5 +10,5 @@ export default defineConfig(({ command }) => ({
       },
     }),
   ],
-  base: command === 'serve' ? '/' : '/frogpath/',
+  base: '/react-shop/',
 }))
